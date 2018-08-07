f26a8fae4faa00080a6f5d91823cc22c4b591006
rakan19-meet <rakan19@meet.mit.edu>
2018-08-07 09:32:19 -0400

    day8 end

--- exercises/app.py
+++ exercises/app.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    pass
+    players_list=["Lebron", "Kyrie", "Kobe", "James Harden"]
+    for i in players_list[]:
+        print(i)
+    return render_template(
+        "index.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
